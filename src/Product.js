8b869a8cd5d48eeea142040bae6f8d613c3f3ffb
John Doe <50689568+dcujoe@users.noreply.github.com>
2022-10-11 02:25:59 +0200

    updates

--- src/Product.js
+++ src/Product.js
@@ -1,16 +1,24 @@
 import './Product.css'
 import React from 'react';
 
-export default function Product() {
+
+
+export default function Product({id, title, image, price, rating}) {
     return (
         <div className="product">
             <div className="product_info">
-                <p>Designer bag made straight from Bonn. Quality at its best</p>
-                <p className="product_price">$30</p>
-                <small></small>
-                <div className="product_rating"></div>
+                <p>{title}</p>
+                <p className="product_price">
+                <small>€</small>
+                <strong>{price}</strong>
+                </p>
+                <div className="product_rating">
+                {Array(rating)
+                .fill()
+                .map((_, i) => (<p></p>))}
+                </div>
             </div>
-            <img src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg" 
+            <img src={image} 
             alt="product_image" />
             <button>Add to basket</button>
         </div>
