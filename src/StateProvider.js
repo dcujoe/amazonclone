035e43819b36f4edd2e4fabb3799c2d729c84571
John Doe <50689568+dcujoe@users.noreply.github.com>
2022-10-14 00:03:07 +0200

    Update StateProvider.js

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -6,9 +6,7 @@ export const StateContext = createContext();
 // Wrap our app and provide the Data layer
 export const StateProvider = ({ reducer, initialState, children }) => (
   <StateContext.Provider value={useReducer(reducer, initialState)}>
-    {children() => {
-        
-    }}
+    {children}
   </StateContext.Provider>
 );
 
