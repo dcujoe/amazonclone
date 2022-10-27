a8cd64ab58e1366258b98b0c9ca71499fc90867e
John Doe <50689568+dcujoe@users.noreply.github.com>
2022-10-27 08:05:40 +0200

    updates

--- src/Header.js
+++ src/Header.js
@@ -26,7 +26,9 @@ const Header = ({ id, title, image, price }) => {
         <div className="header">
           <Link to="/" style={{ textDecoration: "none" }}>
         <div className="header_logo">
-        <SellIcon className="headerLogo_image" fontSize="large" />
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M15.528 2.973a.75.75 0 0 1 .472.696v8.662a.75.75 0 0 1-.472.696l-7.25 2.9a.75.75 0 0 1-.557 0l-7.25-2.9A.75.75 0 0 1 0 12.331V3.669a.75.75 0 0 1 .471-.696L7.443.184l.004-.001.274-.11a.75.75 0 0 1 .558 0l.274.11.004.001 6.971 2.789Zm-1.374.527L8 5.962 1.846 3.5 1 3.839v.4l6.5 2.6v7.922l.5.2.5-.2V6.84l6.5-2.6v-.4l-.846-.339Z"/>
+</svg>
        <h2 className="header_logoTitle">Kode49</h2>
         </div>
           </Link>
@@ -49,7 +51,9 @@ const Header = ({ id, title, image, price }) => {
         </div>
         <Link to="/Checkout" style={{ textDecoration: "none" }}>
         <div className="nav-itemBasket">
-        <ShoppingBasketIcon fontSize="large" />
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
+  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
+</svg>
         <span className="nav_itemLinetwo nav_itemBasketcount" onClick={addToBasket}>{basket.length}</span>
         </div>
         </Link>
