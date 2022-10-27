66705eade254da3fb5ff1d00dfb3168a6cf110d9
John Doe <50689568+dcujoe@users.noreply.github.com>
2022-10-27 06:50:17 +0200

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -10,25 +10,6 @@ import firebase from 'firebase/compat/app';;
 
 
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyBza4axcxgbiKgH8oO3UAiuyFRcniHXVp8",
-    authDomain: "kode49-1ee52.firebaseapp.com",
-    projectId: "kode49-1ee52",
-    storageBucket: "kode49-1ee52.appspot.com",
-    messagingSenderId: "973845219775",
-    appId: "1:973845219775:web:f65490776580da453e390a",
-    measurementId: "G-V1YGKD8VSM"
-  };
-
-
-  
- 
-
-  
-
-
-
 
 function Login() {
     
