6c31ba8159dda5aa9b4a62b1e3bbdf0a17896854
John Doe <50689568+dcujoe@users.noreply.github.com>
2022-11-10 08:26:47 +0100

    footer section

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        );
+    }
+}
+
+export default Footer;
