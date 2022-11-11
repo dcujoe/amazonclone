184e7dc75468e10c73c0d608569559ac6d84b941
John Doe <50689568+dcujoe@users.noreply.github.com>
2022-11-11 22:05:12 +0100

    updates

--- src/Footer.js
+++ src/Footer.js
@@ -3,12 +3,40 @@ import './Footer.css';
 const Footer = () => {
      
         return (
+            <>
+            <div className="back-to-top">Back to top</div>
             <div className="footer">
-                <div></div>
-                <div></div>
+                <div className="get to know">
+                    <h2>Get to know us</h2>
+                    <a>Careers</a>
+                    <a>Press Releases</a>
+                    <a>About us</a>
+                    <a>Blog</a>
+                    <a>Sustainability</a>
+                    <a>Amazon logisticblog</a>
+                    <a>Imprint</a>
+                    <a>Amazon Science</a>
+                </div>
+                <div>
+                <h2>Make money with us</h2>
+                <a>Careers</a>
+                    <a>Sell on Amazon</a>
+                    <a>Sell on amazon business</a>
+                    <a>Sell on Amazon Handmade</a>
+                    <a>Associates Programme</a>
+                    <a>Fulfillment by Amazon</a>
+                    <a>Protect and build your brand</a>
+                    <a>Seller Fulfilled Prime</a>
+                    <a>Advertise your products</a>
+                    <a>Independently Publish with Us</a>
+                    <a>Amazon Pay</a>
+                    <a>Host an Amazon Hub</a>
+                    <a>See More Make Money with Us</a>
+                </div>
                 <div></div>
                 <div></div>
             </div>
+            </>
         );
     }
 
