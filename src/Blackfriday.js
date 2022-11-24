90a44190e0d7fb0ef624865a10ac83c7d2aab45b
John Doe <50689568+dcujoe@users.noreply.github.com>
2022-11-24 23:19:16 +0100

    updates

---
+++ src/Blackfriday.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Blackfriday = () => {
+    return (
+        <div className="black-strip" style={{ color: #fff, marginLeft: 50% }}>
+            Today is black friday!!!
+        </div>
+    );
+};
+
+
+Blackfriday.propTypes = {
+
+};
+
+
+export default Blackfriday;
